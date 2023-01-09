7969746bcbcb791e36ef5670842c564c2d528d4e
Ксения Андреева <kseniadakiny@mail.ru>
2023-01-09 14:48:50 +0300

    Good job 1.07

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
-from yatube.posts.models import Group, Post
-
-# from ..models import Group, Post
+from ..models import Group, Post
 
 User = get_user_model()
 
